65e2dcc79f24a3b1268f167eceb57bd0eba062b2
Lloyd Blake <treleven.lloyd@gmail.com>
2019-08-24 17:31:45 +0100

    first commit

---
+++ Storm-Viewer/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  Storm-Viewer
+//
+//  Created by Lloyd Blake on 24/08/2019.
+//  Copyright © 2019 Lloyd Blake. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToload = selectedImage{
+            imageView.image = UIImage(named: imageToload)
+        }
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+}
